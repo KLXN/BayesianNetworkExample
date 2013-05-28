b4468bb0f00d379a8f147583efbc2ea0b6ed3ab7
Marek Kendziorek <marek@kendziorek.pl>
2013-05-28 13:32:40 +0200

    exercise changed to target assignment

--- bayesianNetworkExample.m
+++ bayesianNetworkExample.m
@@ -8,7 +8,7 @@ clear all;
 close all;
 
 %% Parameters
-N = 4; %number of nodes
+N = 8; %number of nodes
 
 % nodes' IDs
 nodeA = 1; 
@@ -20,21 +20,23 @@ nodeF = 6;
 nodeG = 7;
 nodeH = 8;
 
-% nodes CPDs
-nodeACpd = [0.5 0.5];
-nodeBCpd = [0.5 0.9 0.5 0.1];
-nodeCCpd = [0.8 0.2 0.2 0.8];
-nodeDCpd = [1 0.1 0.1 0.01 0 0.9 0.9 0.99];
-% nodeECpd =
-% nodeFCpd =
-% nodeGCpd =
-% nodeHCpd =
+nodeACpd = [0.99 0.01];
+nodeBCpd = [0.99 0.95 0.01 0.05];
+nodeCCpd = [0.5 0.5];
+nodeDCpd = [0.99 0.9 0.01 0.1];
+nodeECpd = [0.7 0.4 0.3 0.6];
+nodeFCpd = [1 0 0 0 0 1 1 1];
+nodeGCpd = [0.95 0.02 0.05 0.98];
+nodeHCpd = [0.9 0.3 0.2 0.1 0.1 0.7 0.8 0.9];
 
 %% Creating dag (direct acyclic graph)
 dag = zeros(N,N); %matrix of connections
-dag(nodeA,[nodeC nodeB]) = 1;
-dag(nodeC,nodeD) = 1;
-dag(nodeB,nodeD)=1;
+dag(nodeA,nodeB) = 1;
+dag(nodeB,nodeF) = 1;
+dag(nodeC,[nodeD nodeE])=1;
+dag(nodeD,nodeF) = 1;
+dag(nodeE,nodeH) = 1;
+dag(nodeF,[nodeG nodeH]) = 1;
 
 %% Defining nodes types and sizes (in this case all nodes are binary)
 discrete_nodes = 1:N;
@@ -49,19 +51,32 @@ bnet.CPD{nodeA} = tabular_CPD(bnet, nodeA, nodeACpd);
 bnet.CPD{nodeB} = tabular_CPD(bnet, nodeB, nodeBCpd);
 bnet.CPD{nodeC} = tabular_CPD(bnet, nodeC, nodeCCpd);
 bnet.CPD{nodeD} = tabular_CPD(bnet, nodeD, nodeDCpd);
+bnet.CPD{nodeE} = tabular_CPD(bnet, nodeE, nodeECpd);
+bnet.CPD{nodeF} = tabular_CPD(bnet, nodeF, nodeFCpd);
+bnet.CPD{nodeG} = tabular_CPD(bnet, nodeG, nodeGCpd);
+bnet.CPD{nodeH} = tabular_CPD(bnet, nodeH, nodeHCpd);
 
 %% Choosing junction tree engine for inference
 engine = jtree_inf_engine(bnet);
 
 %% Defining observed evidence
 evidence = cell(1,N);
-evidence{nodeD} = 2; %false = 1; true = 2; We observed that this node is true;
+%evidence{nodeD} = 2; %false = 1; true = 2; We observed that this node is true;
+
+%specific problem definition
+evidence{nodeA} = 1;
+%evidence{nodeB} = 2;
 evidence{nodeC} = 2;
+% evidence{nodeD} = 2;
+% evidence{nodeE} = 2;
+% evidence{nodeF} = 2;
+evidence{nodeG} = 2;
+evidence{nodeH} = 1;
 
 %% Connecting evidence and engine
 [engine, loglik] = enter_evidence(engine, evidence);
 
 %% Computing probability
 marg = marginal_nodes(engine, nodeB); %We are looking for case when this node is true
-pFalse = marg.T(1) %probability that it's not true
+pFalse = marg.T(1); %probability that it's not true
 pTrue = marg.T(2) % probability that it's true
