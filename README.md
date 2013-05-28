7c34c3a926cd579c3f60231e285c7a6cab5ee817
Marek Kendziorek <marek@kendziorek.pl>
2013-05-28 10:30:48 +0200

    Implemented example with wet grass

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 BayesianNetworkExample
 ======================
 
-Example of Bayesian Network using BNT
+Example of Bayesian Network using Bayes Net Toolbox (https://code.google.com/p/bnt/)
--- bayesianNetworkExample.m
+++ bayesianNetworkExample.m
@@ -4,6 +4,9 @@
 %
 %
 
+clear all;
+close all;
+
 %% Parameters
 N = 4; %number of nodes
 
@@ -17,6 +20,15 @@ nodeF = 6;
 nodeG = 7;
 nodeH = 8;
 
+% nodes CPDs
+nodeACpd = [0.5 0.5];
+nodeBCpd = [0.5 0.9 0.5 0.1];
+nodeCCpd = [0.8 0.2 0.2 0.8];
+nodeDCpd = [1 0.1 0.1 0.01 0 0.9 0.9 0.99];
+% nodeECpd =
+% nodeFCpd =
+% nodeGCpd =
+% nodeHCpd =
 
 %% Creating dag (direct acyclic graph)
 dag = zeros(N,N); %matrix of connections
@@ -31,3 +43,25 @@ node_sizes = 2*ones(1,N);
 %% Creating Bayes net
 onodes = []; %observed nodes - for now default value
 bnet = mk_bnet(dag, node_sizes, 'discrete', discrete_nodes, 'observed', onodes);
+
+%% Defininf CPD (Conditional Probability Distribution)
+bnet.CPD{nodeA} = tabular_CPD(bnet, nodeA, nodeACpd);
+bnet.CPD{nodeB} = tabular_CPD(bnet, nodeB, nodeBCpd);
+bnet.CPD{nodeC} = tabular_CPD(bnet, nodeC, nodeCCpd);
+bnet.CPD{nodeD} = tabular_CPD(bnet, nodeD, nodeDCpd);
+
+%% Choosing junction tree engine for inference
+engine = jtree_inf_engine(bnet);
+
+%% Defining observed evidence
+evidence = cell(1,N);
+evidence{nodeD} = 2; %false = 1; true = 2; We are looking
+
+%% Connecting evidence and engine
+[engine, loglik] = enter_evidence(engine, evidence);
+
+%% Computing probability
+marg = marginal_nodes(engine, nodeB);
+marg.T
+pFalse = marg.T(1); %probability that it's not true
+pTrue = marg.T(2); % probability that it's true
